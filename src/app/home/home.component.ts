7f3d88be01d3d0bb55959bb12b93579a324ccfd0
Mario Rosa <mariosergiodublin@gmail.com>
2021-07-12 12:29:39 +0100

    First commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number = 0;
+  btnText: string = 'Add an item';
+  goalText: string = 'My first life goal';
+  goals: Array<String> = [];
+
+  constructor(private _data: DataService) { }
+
+  ngOnInit(): void {
+    this._data.goal.subscribe(res => this.goals = res);
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+  }
+
+}
